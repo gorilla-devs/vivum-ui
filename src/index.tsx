09e09a50d0629e6707a03fc13700340adca57a61
Davide <davide.ceschia@gmail.com>
2022-07-08 13:18:03 +0200

    chore: updated dependencies

--- src/index.tsx
+++ src/index.tsx
@@ -5,11 +5,15 @@ import Input from "@ui/Input";
 import "@fontsource/ubuntu";
 // eslint-disable-next-line no-restricted-imports
 import "../index.css";
+import Select from "@ui/Select";
+
+const options = new Array(300).fill(0).map((v, i) => `Number ${i}`);
 
 const Demo = () => (
   <div>
     <Button>{"Button"}</Button>
     <Input />
+    <Select options={options} />
   </div>
 );
 
